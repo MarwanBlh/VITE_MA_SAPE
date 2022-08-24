8c4682b0157a82a826b31c28a1d4d9242fa01267
raphael hamou <hamou.raphael@gmail.com>
2022-08-24 12:23:29 +0200

    add seeds

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,5 +6,5 @@ class Booking < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :clothe, presence: true, uniqueness: true
-  validates :renter, presence: true, uniqueness: true
+  validates :renter, presence: true
 end
