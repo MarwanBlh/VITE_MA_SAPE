a480c003511931f8c589ae7be8b88151039868e0
raphael hamou <hamou.raphael@gmail.com>
2022-08-26 13:12:17 +0200

    create a clothe

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -2,8 +2,25 @@ class ClothesController < ApplicationController
   def show
     @clothe = Clothe.find(params[:id])
   end
-  
+
   def index
     @clothes = Clothe.all
+  end
+
+  def new
+    @clothe = Clothe.new
+  end
+
+  def create
+    @clothe = Clothe.new(params_clothe)
+    @clothe.owner = current_user
+    @clothe.save # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to dresser_menu_path
+  end
+
+  private
+
+  def params_clothe
+    params.require(:clothe).permit(:title, :description, :address, :daily_price, :gender, :size, :category, :color, :photo)
   end
 end
