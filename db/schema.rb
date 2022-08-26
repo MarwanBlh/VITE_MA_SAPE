578a86456c8a23dc948f94e58993334a23054e73
Thomas Perez <t.perez@audencia.com>
2022-08-26 14:40:01 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_26_103135) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_26_111215) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_26_103135) do
     t.bigint "clothe_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.date "end_date"
     t.index ["clothe_id"], name: "index_bookings_on_clothe_id"
     t.index ["renter_id"], name: "index_bookings_on_renter_id"
